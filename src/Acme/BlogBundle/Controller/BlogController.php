75d401e84a9535e100167c75f9e30ba1ad030797
petrero <petrero@gmail.com>
2014-01-28 21:23:46 +0200

    create functionality for show a blog

---
+++ src/Acme/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,34 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Acme\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Blog controller
+ */
+
+class BlogController extends Controller{
+    /**
+     * Show a blog entry
+     */
+    public function showAction($id){
+        $em = $this->getDoctrine()->getManager();
+        
+        $blog = $em->getRepository('AcmeBlogBundle:Blog')->find($id);
+        
+        if (!$blog){
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+        
+        return $this->render('AcmeBlogBundle:Blog:show.html.twig', array(
+            'blog' => $blog
+        ));
+    }
+}
+
