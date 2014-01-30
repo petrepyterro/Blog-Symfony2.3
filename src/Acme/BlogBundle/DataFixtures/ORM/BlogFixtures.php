06d50057669bfa474f4f103364ed5672fd6540f6
petrero <petrero@gmail.com>
2014-01-30 18:23:14 +0200

    set references to objects in BlogFixture that CommentFixture can access and implement getOrder method in CommentFixture to setr the return value to 2 ensuring CommentFixture will be loaded after BlogFixture and then run in console 'php app/console doctrine:fixtures:load'

--- src/Acme/BlogBundle/DataFixtures/ORM/BlogFixtures.php
+++ src/Acme/BlogBundle/DataFixtures/ORM/BlogFixtures.php
@@ -7,11 +7,12 @@
  */
 namespace Acme\BlogBundle\DataFixtures\ORM;
 
-use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Acme\BlogBundle\Entity\Blog;
 
-class BlogFixtures implements FixtureInterface{
+class BlogFixtures extends AbstractFixture implements OrderedFixtureInterface{
     public function load(ObjectManager $manager) {
         $blog1 = new Blog();
         $blog1->setTitle('A day with Symfony 2');
@@ -65,6 +66,16 @@ class BlogFixtures implements FixtureInterface{
         
         
         $manager->flush();
+        
+        $this->addReference('blog-1', $blog1);
+        $this->addReference('blog-2', $blog2);
+        $this->addReference('blog-3', $blog3);
+        $this->addReference('blog-4', $blog4);
+        $this->addReference('blog-5', $blog5);
+    }
+    
+    public function getOrder() {
+        return 1;
     }
 }
 
