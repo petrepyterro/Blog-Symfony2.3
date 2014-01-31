92e29cbd40d725eaefc0877b7775577486f274a0
petrero <petrero@gmail.com>
2014-01-31 19:26:05 +0200

    we need to create a form on the blog show and process it so we must create a controller for Comment Entity; after creating a comment we want to redirect to blog show page displayed around the location of the added comment

---
+++ src/Acme/BlogBundle/Controller/CommentController.php
@@ -0,0 +1,67 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Acme\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Acme\BlogBundle\Entity\Comment;
+use Acme\BlogBundle\Form\CommentType;
+
+/**
+ * Comment controller
+ */
+
+class CommentController extends Controller{
+    public function newAction($blog_id) {
+        $blog = $this->getBlog($blog_id);
+        
+        $comment = new Comment();
+        $comment->setBlog($blog);
+        $form = $this->createForm(new CommentType(), $comment);
+        
+        return $this->render('AcmeBlogBundle:Comment:form.html.twig', array(
+            'comment' => $comment,
+            'form' => $form->createView()
+        ));
+    }
+    
+    public function createAction($blog_id) {
+        $blog = $this->getBlog($blog_id);
+        
+        $comment = new Comment();
+        $comment->setBlog($blog);
+        $request = $this->getRequest();
+        $form = $this->createForm(new CommentType(), $comment);
+        
+        $form->handleRequest($request);
+        
+        if ($form->isValid()){
+            return $this->redirect($this->generateUrl('acme_blog_blog_show', array(
+                'id' => $comment->getBlog()->getId())).
+                '#comment-' . $comment->getId()   
+            );
+        }
+        
+        return $this->render('AcmeBlogBundle:Comment:create.html.twig', array(
+            'comment' => $comment,
+            'form' => $form->createView()
+        ));
+    }
+    
+    protected function getBlog($blog_id) {
+        $em = $this->getDoctrine()->getManager();
+        
+        $blog = $em->getRepository('AcmeBlogBundle:Blog')->find($blog_id);
+        
+        if (!$blog){
+            throw new $this->createNotFoundException('Unable to find Blog post.');
+        }
+        
+        return $blog;
+    }
+}
+
